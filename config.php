be872d48e1e8512d46df151c5b4e7289cb50a14c
msstngr <80616356+msstngr@users.noreply.github.com>
2021-03-15 04:22:52 +0000

    update config to TLD

--- config.php
+++ config.php
@@ -5,4 +5,4 @@ define('DATABASE_SERVER',   'localhost');
 define('DATABASE_USERNAME', 'fanadigi_nas');
 define('DATABASE_PASSWORD', '@Vc9bnr212134');
 define('DATABASE_NAME',     'fanadigi_linkpost');
-define('SITE_URL',          'http://linkpost.fana.digital/');
+define('SITE_URL',          'http://linkpo.st/');
